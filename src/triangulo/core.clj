6a412b9a8d93c1f8935a5f28ace26f1838aa86c5
Caio Santos <caio.santos@caio.santos>
2022-09-16 15:49:44 -0300

    resolving triangle issue valuable

--- src/triangulo/core.clj
+++ src/triangulo/core.clj
@@ -6,56 +6,69 @@
   [a b c]
   (+ a b c))
 
+(defn angulo
+  "Calcula o angulo"
+  [a b c]
+  (math/acos (/ (- (+ (math/pow b 2)
+                      (math/pow c 2))
+                   (math/pow a 2))
+                (* 2 b c))))
+
 (defn calc-radianos
   "TODO: Calcular radianos dado lados a b e c de um triangulo"
   [a b c]
-  )
+  (math/to-radians (angulo a b c)))
 
 (defn calc-angulo
   "TODO: Calcula o ângulo ∠A, dado A B C."
   [a b c]
-  )
+  (math/to-degrees (angulo a b c)))
 
 (defn calc-area
   "TODO: Calcula a área de um triângulo usando a formula de Heron."
   [a b c]
-  )
+  (let [s (/ (calc-perimetro a b c) 2)]
+    (math/sqrt (* (* (* s (- s a)) (- s b)) (- s c)))))
 
 (defn calc-altura
   "TODO: Calcula altura de A, dado a AREA."
   [a area]
-  )
+  (/ (* 2 area) a))
 
 (defn equilateral?
   "TODO: Verifica se o triangulo é equilateral"
   [a b c]
-  )
+  (== a b c))
 
 (defn isosceles?
   "TODO: Verifica se pelo menos dois lados sao iguais."
   [a b c]
-  )
+  (or (== a b) (== a c) (== b c)))
 
 (defn escaleno?
   "TODO: Verifica se os lados dos triangulos sao diferentes entre si."
   [a b c]
-  )
+  (not (== a b c)))
 
 (defn retangulo?
   "TODO: Verifica se é um triangulo retangulo, cujos angulos são iguais a 90o.
   O resultado não é exato, dado que cada angulo é arredondado utilizando clojure.math/round."
   [a b c]
-  )
+  (or (= (math/round (calc-angulo a b c)) 90) (= (math/round (calc-angulo b c a)) 90) (= (math/round (calc-angulo c b a)) 90)))
 
 (defn obtuso?
   "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (if (not (retangulo? a b c))
+    (or (> (math/round (calc-angulo a b c)) 90) (> (math/round (calc-angulo b c a)) 90) (> (math/round (calc-angulo c a b)) 90))
+    false))
 
 (defn agudo?
   "TODO: Verifica se o triangulo é obtuso, tendo algum angulo >90o."
   [a b c]
-  )
+  (if (not (retangulo? a b c))
+    (and (< (math/round (calc-angulo a b c)) 90) (< (math/round (calc-angulo b c a)) 90) (< (math/round (calc-angulo c a b)) 90))
+    false))
 
 (defn gerar-dados-completos
   [a b c]
